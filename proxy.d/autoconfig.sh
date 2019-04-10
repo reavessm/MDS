4028f3ba2a6b21e6a51e995e9bd5dd57c4011f21
Stephen Reaves <reaves735@gmail.com>
2019-04-09 21:19:43 -0400

    :rocket: Reverse proxy autogenerates with HTTPS/SSL

--- proxy.d/autoconfig.sh
+++ proxy.d/autoconfig.sh
@@ -8,7 +8,8 @@ else
     0 0 --stdout`
 fi
 
-file="nginx.conf"
+file="config/nginx/site-confs/default"
+rm "$file"
 
 # Make sure website goes first
 subs="website:`awk -F '=' '/exposedPort/ {print $2}' ../website.d/mds.sh` "
@@ -23,19 +24,6 @@ do
   fi
 done
 
-# Basic stuff
-cat > $file <<EOF
-worker_processes 5;
-
-events {
-    worker_connections 1024;
-}
-
-http {
-    sendfile on;
-
-EOF
-
 # Upstream servers
 # TODO: allow for multiple hosts
 for sub in $subs
@@ -43,35 +31,83 @@ do
   name="`echo ${sub} | cut -d ':' -f1`"
   ip="`echo ${sub} | cut -d ':' -f2`"
   cat >> $file <<EOF
-    upstream ${name}_server {
-        server 192.168.0.3:${ip};
-    } 
+upstream ${name}_server {
+    server 192.168.0.3:${ip};
+} 
 
 EOF
 done
+
+# Basic stuff
+cat >> $file <<EOF
+server {
+    listen 80;
+    listen [::]:80;
+    allow  all;
+    return 301 https://\$host\$request_uri;
+}
+
+server {
+    listen 443 ssl http2 default_server;
+    listen [::]:443 ssl http2 default_server;
+
+    root /config/www;
+    index index.html index.htm index.php;
+
+    server_name _;
+
+    ssl_session_timeout 1d;
+    ssl_session_cache shared:SSL:50m;
+    ssl_session_tickets off;
+
+    ssl_dhparam /config/nginx/dhparams.pem;
+
+    ssl_certificate /config/keys/letsencrypt/fullchain.pem;
+    ssl_certificate_key /config/keys/letsencrypt/privkey.pem;
+
+    ssl_protocols TLSv1.2 TLSv1.3;
+    ssl_prefer_server_ciphers on;
+    ssl_ciphers 'ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256';
+
+    ssl_stapling on;
+    ssl_stapling_verify on;
+    resolver 8.8.8.8 8.8.4.4;
+
+    location / {
+        #try_files \$uri \$uri/ /index.html /index.php?\$args =404;
+        proxy_pass http://website_server;
+          
+        proxy_set_header Host              \$host;
+        proxy_set_header X-Real-IP         \$remote_addr;
+        proxy_set_header X-Forwarded-For   \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Host  \$server_name;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+}
+
+EOF
 
 # Location directives
 for sub in $subs
 do
   name="`echo ${sub} | cut -d ':' -f1`"
   cat >> $file <<EOF
-    server {
-        listen 80;
-        allow  all;
-        server_name ${name}.${dom} www.${name}.${dom};
+server {
+    listen 443;
+    listen [::]:443;
+    allow  all;
+    server_name ${name}.${dom} www.${name}.${dom} ${name}.*;
 
-        location / {
-            proxy_pass http://${name}_server;
-            
-            proxy_set_header Host             \$host;
-            proxy_set_header X-Real-IP        \$remote_addr;
-            proxy_set_header X-Forwarded-For  \$proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Host \$server_name;
-        }
+    location / {
+        proxy_pass http://${name}_server;
+          
+        proxy_set_header Host              \$host;
+        proxy_set_header X-Real-IP         \$remote_addr;
+        proxy_set_header X-Forwarded-For   \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Host  \$server_name;
+        proxy_set_header X-Forwarded-Proto \$scheme;
     }
+}
 
 EOF
 done 
-
-# End http block
-echo "}" >> $file
--- proxy.d/mds.sh
+++ proxy.d/mds.sh
@@ -2,13 +2,34 @@
 
 [ -f ../mds.sh ] && source ../mds.sh || exit 1
 
-conName="proxy"
+conName="rev-proxy"
 
-conImg="proxy"
+#conImg="proxy"
 
-# Make sure you put a space at the beginning of every arg EXCEPT the first
+## Make sure you put a space at the beginning of every arg EXCEPT the first
+#args="-d"
+#args+=" -p 80:80"
+
+conImg="linuxserver/letsencrypt"
+#conImg="proxy"
+
 args="-d"
+args+=" -e PUID=1001"
+args+=" -e PGID=1001"
+args+=" -e TZ=America/New_York"
+args+=" -e URL=reaves.dev"
+args+=" -e SUBDOMAINS=www,`ls .. | awk -F '.' \
+  'BEGIN{ORS=","} /\.d/ {print $1}' | sed 's/,$//'`"
+args+=" -e EMAIL=reaves735@gmail.com"
+args+=" -e DHLEVEL=1024"
+args+=" -e VALIDATION=http"
+#args+=" -e VALIDATION=dns"
+#args+=" -e DNSPLUGIN=cloudflare"
+args+=" -p 443:443"
 args+=" -p 80:80"
+args+=" -v `pwd`/config:/config:rw"
+#args+=" -v nginx.conf:/config/nginx/site-confs/default"
+
 
 # run args
 $1
