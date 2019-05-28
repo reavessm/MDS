c5a11f6f6a5732fba1f20408306d42bf43e0247a
Stephen Reaves <reaves735@gmail.com>
2019-05-28 08:33:53 -0400

    :sparkle: Added more examples

--- mds.sh
+++ mds.sh
@@ -161,27 +161,27 @@ function new() {
 # Source the parent mds.sh
 [ -f ../mds.sh ] && source ../mds.sh || exit 1
 
-# You must specify container name
+# You must specify container name.
 conName="$name"
 
-# You must specify a container image
+# You must specify a container image.
 conImg="$img"
 
 # If your container does not need a separate DB or network, leave these
-# commented out
+# commented out.
 #conDB="\$conName-DB"
 #conDBImg="mariadb"
 #conNet="\$conName-net"
 
-# Put the port you want to be made public to the load balancer
+# Put the port you want to be made public to the load balancer.
 #exposedPort=8082
 
-# Put the IP of the host of the vm if not managed by MDS
-# Normally, it's safe to ignore this
+# Put the IP of the host of the vm if not managed by MDS.
+# Normally, it's safe to ignore this.
 #conIP=192.168.0.0
 
 # Use this block to prompt for usernames and passwords, but only if there is
-# no container named conName
+# no container named conName.
 #if [ -z "\`docker ps -a | awk '{print \$NF}' | grep -x \$conName\`" ]
 #then
 #  read -p "Please enter \$conName username: " username
@@ -190,7 +190,7 @@ conImg="$img"
 #fi
 
 # These are the args passed to the \`docker run\` command.  Make sure all args
-# EXCEPT for the first one start with a space
+# EXCEPT for the first one start with a space.
 args="-d"
 EOF
 
@@ -218,15 +218,15 @@ done
 
 cat >> $name.d/mds.sh << EOF
 
-# If you need to group things in a network
+# If you need to group things in a network:
 #args+=" --net \$conNet"
 
-# If you need a specific username and password
+# If you need a specific username and password:
 #args+=" -e KEYCLOAK_USER=\$username"
 #args+=" -e KEYCLOAK_PASSWORD=\$password"
 
 # These are the args passed to the \`docker run\` command for the DB, if conDB is
-# not blank.  Make sure all args EXCEPT for the first one start with a space
+# not blank.  Make sure all args EXCEPT for the first one start with a space.
 #dbArgs="-d"
 #dbArgs+=" --net \$conNet"
 #dbArgs+=" -e MYSQL_ROOT_PASSWORD=password"
@@ -239,7 +239,7 @@ cat >> $name.d/mds.sh << EOF
 #function preconfig() {
 #  print "Doing something before run ..."
 #  echo Something
-#  printRed "Done something for $conName!"
+#  printRed "Done something for \$conName!"
 #}
 
 # Uncomment this to run commands after the \`docker run\` command.  These
@@ -247,10 +247,28 @@ cat >> $name.d/mds.sh << EOF
 #function postconfig() {
 #  print "Doing after before run ..."
 #  echo Something
-#  printRed "Done something for $conName!"
+#  printRed "Done something for \$conName!"
+
+# Ovewrite these methods for vms not managed in MDS.  The proxy will still
+# point to the service, but will not create it.  This is normally used with the
+# conIp variable to specify that the VM is on a different host.
+#function run() {
+#  print "\$conName is not managed by MDS"
+#}
+#
+#function stop() {
+#  print "\$conName is not managed by MDS"
+#}
+#
+#function remove() {
+#  print "\$conName is not managed by MDS"
+#}
+#
+#function start() {
+#  print "\$conName is not managed by MDS"
 #}
 
-# Run args.  Do not delete this deceptively simple command
+# Run args.  Do NOT delete this deceptively simple command.
 \$1
 EOF
 
