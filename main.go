b8cb460cdb0f36a4a7363fd11268569f5dd84670
Stephen Reaves <reaves735@gmail.com>
2019-12-06 08:13:57 -0500

    :construction: Started main shell

--- main.go
+++ main.go
@@ -7,57 +7,57 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "strings"
-  "bufio"
-  "regexp"
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
 )
 
-var prompt string;
+var prompt string
 
 func prepareArg(match [][]string) {
-  arg   := match[0][1]
-  value := match[1][2]
+	arg := match[0][1]
+	value := match[1][2]
 
-  switch arg {
-    case `prompt`, `PS1`:
-      prompt = value
-  }
+	switch arg {
+	case `prompt`, `PS1`:
+		prompt = value
+	}
 }
 
 func execInput(input string) {
-  // Remove new line
-  input = strings.TrimSuffix(input, '\n')
+	// Remove new line
+	input = strings.TrimSuffix(input, "\n")
 }
 
 func main() {
-  // Defaults
-  prompt = "> "
-  match := regexp.MustCompile(`^--([[:alnum:]]+)=|(.*)$`)
-  reader := bufio.NewReader(os.Stdin)
+	// Defaults
+	prompt = "> "
+	match := regexp.MustCompile(`^--([[:alnum:]]+)=|(.*)$`)
+	reader := bufio.NewReader(os.Stdin)
 
-  // Handle commandline arguments
-  for i, arg := range os.Args {
-    // Arg[0] is name of function
-    if i > 0 {
-      argMatch := match.FindAllStringSubmatch(arg, -1)
-      prepareArg(argMatch)
-    }
-  }
+	// Handle commandline arguments
+	for i, arg := range os.Args {
+		// Arg[0] is name of function
+		if i > 0 {
+			argMatch := match.FindAllStringSubmatch(arg, -1)
+			prepareArg(argMatch)
+		}
+	}
 
-  // Shell
-  for/*ever*/ {
-    // Read input
-    input, err := reader.ReadString('\n')
-    if err != nil {
-      fmt.Fprintln(os.Stderr, err)
-    }
+	// Shell
+	for /*ever*/ {
+		// Read input
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
 
-    // Prompt
-    fmt.Print(prompt)
+		// Prompt
+		fmt.Print(prompt)
 
-    execInput(input)
-    os.Exit(0)
-  }
+		execInput(input)
+		os.Exit(0)
+	}
 }
